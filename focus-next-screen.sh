f12ec0af79fddfc9537af3d60f93e461886d616f
ik9999 <ik9999@users.noreply.github.com>
2023-01-17 19:09:27 +0400

    Initial commit

---
+++ focus-next-screen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+screen_width=$(xdpyinfo | awk '/dimensions:/ { print $2; exit }' | cut -d"x" -f1)
+display_width=$(xdotool getdisplaygeometry | cut -d" " -f1)
+
+current_x=$(xdotool getmouselocation --shell | head -1 | cut -d"=" -f 2)
+
+new_x=$((current_x + display_width))
+new_y=$(xdotool getmouselocation --shell | head -2 | tail -1 | cut -d"=" -f 2)
+
+if [ "$new_x" -gt "$screen_width" ]; then
+  new_x=$(($new_x - $screen_width))
+fi
+
+new_window_id=$(xdotool mousemove "$new_x" "$new_y" getmouselocation | cut -d" " -f 4 | cut -d":" -f 2)
+
+if [[ $new_window_id == ?(-)+([[:digit:]]) ]]; then
+  xdotool windowfocus "$new_window_id"
+fi
